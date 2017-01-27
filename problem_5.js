7eade8ef57ef656093192ea830475425d21b974d
Jeremy Kithome <jeremy.kithome@andela.com>
2017-01-27 11:22:07 +0300

    Add soultion for problem 5

---
+++ problem_5.js
@@ -0,0 +1,15 @@
+function main(num) {
+  var arr = Array.from({ length: num }, (val, index) => index + 1);
+
+  for (var i = 1; i < num; i++) {
+    for (var j = i + 1; j < num; j++) {
+      if (arr[j] % arr[i] === 0) {
+        arr[j] = arr[j] / arr[i];
+      }
+    }
+  }
+  var product = arr.reduce((a, b) => {
+    return a * b;
+  }, 1)
+  return product;
+}
