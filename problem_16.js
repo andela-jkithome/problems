38c992d06e758b1e2389e82a2dfa03730f9a9777
Jeremy Kithome <jeremy.kithome@andela.com>
2017-02-02 12:33:32 +0300

    Optimize problem 16

--- problem_16.js
+++ problem_16.js
@@ -2,28 +2,24 @@
 
 function main(number, exponent) {
   var result = bigSquare(number, exponent);
-  var sum = result.reduce((a, b) => {
-    return a + b }, 0);
+  var sum = result.reduce((a,b) => {return a + b}, 0);
   return sum;
 
 }
-
 function bigSquare(num, pow) {
   var arr = [1];
-  for (var i = 1; i <= pow; i++) {
+  for(var i = 1; i <= pow; i++) {
     var carryOver = 0;
-    for (var j = arr.length - 1; j >= 0; j--) {
+    for(var j = arr.length - 1; j >= 0; j--) {
       var currentNum = arr[j];
-      var newNumber = (num * currentNum) + carryOver;
+      var newNumber  = (num * currentNum) + carryOver;
       if (newNumber >= 10) {
-        var string = newNumber.toString();
-        var numbers = string.split('');
-        arr[j] = parseInt(numbers[1]);
-        if (j === 0) {
-          arr.unshift(parseInt(numbers[0]));
+        arr[j] = newNumber % 10;
+        if(j === 0) {
+          arr.unshift(Math.floor(newNumber / 10));
           break;
         } else {
-          carryOver = parseInt(numbers[0]);
+          carryOver = (Math.floor(newNumber / 10));
         }
       } else {
         arr[j] = newNumber;
